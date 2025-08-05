035d2c16a894477ba71398c3f42c5304e1aaab0a
Joe Shoulak <joepshoulak2@gmail.com>
2025-08-05 15:30:29 -0500

    polish

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Error: Cannot access webcam. Is it in use?")
-    exit()
-
 def draw_reticle(frame):
     RETICLE_COLOR = (0, 0, 255) # red
     RETICLE_SIZE = 5
@@ -19,24 +14,58 @@ def preprocess(frame):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # grayscale
     return cv2.GaussianBlur(frame, (BLUR_SIZE, BLUR_SIZE), SIGMA_X) # blur to reduce noise
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    draw_reticle(frame)
-    preprocessed = preprocess(frame)
-
+def make_contours(frame):
     THRESH_MIN = 25
     THRESH_MAX = 150
-    edges = cv2.Canny(preprocessed, THRESH_MIN, THRESH_MAX)
+    return cv2.Canny(frame, THRESH_MIN, THRESH_MAX)
 
-    cv2.imshow("Original", frame)
-    cv2.imshow("Preprocessed", preprocessed)
-    cv2.imshow("Edges", edges)
+def find_roi(frame):
+    contours, _ = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    height, width = frame.shape[:2]
+    center_point = (width // 2, height // 2)
 
-cap.release()
-cv2.destroyAllWindows()
+    for cnt in contours:
+        if cv2.pointPolygonTest(cnt, center_point, measureDist=False) >= 0:
+            x, y, w, h = cv2.boundingRect(cnt)
+            return (x, y, w, h)
+        
+def draw_roi(frame, roi):
+        ROI_COLOR = (0, 255, 0) # green
+        x, y, w, h = roi
+        cv2.rectangle(frame, (x, y), (x + w, y + h), ROI_COLOR, 2)
+        return frame[y:y + h, x:x + w]
+
+######## MAIN ########
+def main():
+    roi = None
+
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error: Cannot access webcam. Is it in use?")
+        exit()
+
+    while True:
+        ret, frame = cap.read()
+        if not ret: break
+
+        draw_reticle(frame)
+        preprocessed = preprocess(frame)
+        edges = make_contours(preprocessed)
+        cropped = None
+
+        if not roi: roi = find_roi(edges)
+        if roi: cropped = draw_roi(frame, roi)
+
+        cv2.imshow("Original", frame)
+        cv2.imshow("Preprocessed", preprocessed)
+        cv2.imshow("Edges", edges)
+        if cropped is not None: cv2.imshow("Screen ROI", cropped)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
